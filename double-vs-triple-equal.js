2bc9373878d8d346bba04f1249bd35d08b8091a4
Sagor Mahtab <sagormahtab@gmail.com>
2020-03-04 01:32:41 +0600

    double vs triple equal concept

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,29 @@
+// const first = 2;
+// const second = '2';
+// if(first == second){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+// const first = 2;
+// const second = '2';
+// if(first === second){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+
+// == just checks the value not the type, type ke basically kasakasi type e convert kore fele
+// === checks the value plus type
+const first = 1;
+const second = true;
+if(first === second){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
