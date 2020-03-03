3f72d64fd2e36b4bddff2a98d153398d29369ce6
Sagor Mahtab <sagormahtab@gmail.com>
2020-03-04 01:17:33 +0600

    null vs undefined concept

---
+++ null-vs-undefined.js
@@ -0,0 +1,21 @@
+let pakhi;
+function add(num1, num2){
+    console.log(num1 + num2);
+    return;
+}
+function add(num1, num2){
+    console.log(num1, num2);
+}
+const result = add(13);
+
+const premik = {name: "Smart Dude", phone:23534};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+//undefined onek bhabe pawa jay
+//kintu null jinishta amra explicitly set kori
+
+let ages = [2, 5, 9];
+console.log(ages[11]);
